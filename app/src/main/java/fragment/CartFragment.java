95b01e0c5d3f9136c6e3eab5568626f587de1283
letienthanh <ltthanh22112002@gmail.com>
2024-07-20 20:39:25 +0700

    learn git

--- app/src/main/java/fragment/CartFragment.java
+++ app/src/main/java/fragment/CartFragment.java
@@ -16,5 +16,6 @@ public class CartFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cart, container, false);
+        //// learn git
     }
 }
