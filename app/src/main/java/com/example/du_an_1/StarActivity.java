f085fc9ca72d4218306632de5e4227e378651ad9
khang96 <Monsteruk1@>
2024-07-30 12:59:47 +0700

    update

--- app/src/main/java/com/example/du_an_1/StarActivity.java
+++ app/src/main/java/com/example/du_an_1/StarActivity.java
@@ -2,6 +2,7 @@ package com.example.du_an_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class StarActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class StarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_star);
+
+        Intent intent = new Intent(StarActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
