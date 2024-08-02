b96860d0380a1608951dc27b5b9df65e20e3e059
phongnh10 <nguyenhongphong1010.02@gmail.com>
2024-08-02 11:40:37 +0700

    update

--- app/src/main/java/com/example/du_an_1/MainActivity.java
+++ app/src/main/java/com/example/du_an_1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.du_an_1;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -35,11 +36,21 @@ public class MainActivity extends AppCompatActivity {
         checkRole();
 
         Intent intent = getIntent();
-        String fragmentName = intent.getStringExtra("fragment");
+        String fragmentName = null;
+        if (intent.hasExtra("CartBuyFragment")) {
+            fragmentName = intent.getStringExtra("CartBuyFragment");
+        } else if (intent.hasExtra("SearchFragment")) {
+            fragmentName = intent.getStringExtra("SearchFragment");
+        }else if(intent.hasExtra("SettingFragment")){
+            fragmentName = intent.getStringExtra("SettingFragment");
+        }
+
         if (fragmentName != null && !fragmentName.isEmpty()) {
             replaceFragment(getFragmentByName(fragmentName));
+            binding.bottomNavigation.setSelectedItemId(getMenuItemIdByFragmentName(fragmentName));
         } else {
             replaceFragment(new HomeFragment());
+            binding.bottomNavigation.setSelectedItemId(R.id.nav_home);
         }
 
         int role = getRoleFromSharedPreferences();
@@ -71,6 +82,22 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         });
+
+        int[][] states = new int[][] {
+                new int[] { android.R.attr.state_checked }, // trạng thái được chọn
+                new int[] { -android.R.attr.state_checked } // trạng thái không được chọn
+        };
+
+        int[] colors = new int[] {
+                getResources().getColor(R.color.orange),
+                getResources().getColor(R.color.default_color)
+        };
+
+        ColorStateList colorStateList = new ColorStateList(states, colors);
+
+        // Thiết lập màu sắc cho các mục trong BottomNavigationView
+        binding.bottomNavigation.setItemIconTintList(colorStateList);
+        binding.bottomNavigation.setItemTextColor(colorStateList);
     }
 
     // Method to get Fragment by name
@@ -93,6 +120,27 @@ public class MainActivity extends AppCompatActivity {
             case "HomeFragment":
             default:
                 return new HomeFragment();
+        }
+    }
+
+    // Method to get MenuItem ID by Fragment name
+    private int getMenuItemIdByFragmentName(String fragmentName) {
+        switch (fragmentName) {
+            case "CartSellFragment":
+                return R.id.nav_cart;
+            case "CartBuyFragment":
+                return R.id.nav_cart;
+            case "ManageAdminFragment":
+            case "ManageBuyerFragment":
+            case "ManageSellerFragment":
+                return R.id.nav_shop;
+            case "SearchFragment":
+                return R.id.nav_search;
+            case "SettingFragment":
+                return R.id.nav_setting;
+            case "HomeFragment":
+            default:
+                return R.id.nav_home;
         }
     }
 
@@ -124,6 +172,8 @@ public class MainActivity extends AppCompatActivity {
             case 1: // seller
                 break;
             case 2: // buyer
+                MenuItem nav_shop1 = binding.bottomNavigation.getMenu().findItem(R.id.nav_shop);
+                nav_shop1.setVisible(false);
                 break;
             default:
                 break;
