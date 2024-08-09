8497270d66bdfcee8b5ebacef3000b7c0d1cb954
phongnh10 <nguyenhongphong1010.02@gmail.com>
2024-08-09 12:05:31 +0700

    lỗi back đổi mật khẩu

--- app/src/main/java/com/example/du_an_1/LoginActivity.java
+++ app/src/main/java/com/example/du_an_1/LoginActivity.java
@@ -49,13 +49,7 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        txtChangePass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, ChangepassActivity.class);
-                startActivity(intent);
-            }
-        });
+
         txtSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
